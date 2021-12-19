2cc1fc98df1d3f2d3358ea2582b0a96f141ea5a1
Mark77645 <Marksamirf@gmail.com>
2021-12-19 15:25:02 +0200

    Class for processes

--- src/Process.java
+++ src/Process.java
@@ -1,2 +1,47 @@
 public class Process {
+    private String name;
+    private String color;
+    private int burstTime;
+    private int arrivalTime;
+    private int priority;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getBurstTime() {
+        return burstTime;
+    }
+
+    public void setBurstTime(int burstTime) {
+        this.burstTime = burstTime;
+    }
+
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(int arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
 }
